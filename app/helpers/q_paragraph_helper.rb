2f7ba6b2c867248385ceb21d279652917804db72
Andrey Makarov <over.ph@gmail.com>
2011-11-23 17:06:30 +0400

    render_paragraph_block now is recursive

--- app/helpers/q_paragraph_helper.rb
+++ app/helpers/q_paragraph_helper.rb
@@ -7,12 +7,13 @@ module QParagraphHelper
           html << '<div class="q-paragraph-law">'+ unit.body + '</div>'
         when 'Type_QParagraphBlock' then
           html << '<h3>'+unit.title+'</h3>'
-          #unit.outgoing(:consists_of).each { |block_unit| render_paragraph_unit(block_unit) }
-          unit.outgoing(:consists_of).each { |block_unit| html << block_unit.body }
+          unit.outgoing(:consists_of).each { |block_unit| html << render_paragraph_unit(block_unit) }
       end
     else
       html << '<h3>'+unit.title+'</h3>' #TODO remove and change schema by merging blocks
+      html << '<p>'
       html << unit.body
+      html << '</p>'
     end
     html
   end
