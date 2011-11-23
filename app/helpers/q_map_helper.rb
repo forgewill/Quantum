0ac6447e2ed8fb86f28063cc3ef9cc5f69c7d38a
Andrey Makarov <over.ph@gmail.com>
2011-11-23 20:20:16 +0400

    Output simplest track

--- app/helpers/q_map_helper.rb
+++ app/helpers/q_map_helper.rb
@@ -1,9 +1,10 @@
 module QMapHelper
   def render_simple_map(root)
     html = ""
-
+    index = 0
     root.incoming(:need_to_learn).depth(:all).reverse_each do |node|
-      html << "<p>"+node.title+"</p>"
+      index = index + 1
+      html << "<p>"+index.to_s+". "+node.title+"</p>"
     end
 
     html
