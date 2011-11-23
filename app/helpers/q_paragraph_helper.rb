d343c021893a5dd458635ed780b437ae290d7c29
Andrey Makarov <over.ph@gmail.com>
2011-11-23 16:45:17 +0400

    One level paragraph's blocks render via render_paragraph_block helper.

--- app/helpers/q_paragraph_helper.rb
+++ app/helpers/q_paragraph_helper.rb
@@ -1,2 +1,23 @@
 module QParagraphHelper
+  def render_paragraph_unit(unit)
+    html = ""
+    if unit.outgoing(:is_type).first != nil
+      case unit.outgoing(:is_type).first.title
+        when 'Type_QParagraphLaw'
+          html << '<div class="q-paragraph-law">'+ unit.body + '</div>'
+        when 'Type_QParagraphBlock' then
+          html << '<h3>'+unit.title+'</h3>'
+          #unit.outgoing(:consists_of).each { |block_unit| render_paragraph_unit(block_unit) }
+          unit.outgoing(:consists_of).each { |block_unit| html << block_unit.body }
+      end
+    else
+      html << '<h3>'+unit.title+'</h3>' #TODO remove and change schema by merging blocks
+      html << unit.body
+    end
+    html
+  end
 end
+
+#unit.outgoing(:consists_of).each do |block_unit|
+#  block_unit.id
+#end
