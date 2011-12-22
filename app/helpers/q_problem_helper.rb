dd8035cf942e2a408a0d4bbe2b5ab69d24915874
Andrey Makarov <over.ph@gmail.com>
2011-12-22 13:32:39 +0400

    Added simple helper to check right problem solving

--- app/helpers/q_problem_helper.rb
+++ app/helpers/q_problem_helper.rb
@@ -1,2 +1,14 @@
 module QProblemHelper
+  def problem_is_right(problem_id)
+    @problem_right = Neo4j.query("START y=node({problem_id}), u=node({user_id}), ts=node({qsolution_type_id})
+                                  MATCH (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
+                                  RETURN y",
+                                 'problem_id' => problem_id,
+                                 'user_id' => current_user.id.to_i,
+                                 'qsolution_type_id' => 126,
+                                 'qproblem_type_id' => 123)
+
+    #@problem_right
+    true if @problem_right.count > 0
+  end
 end
