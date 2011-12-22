87a8bb580bd7126c90830452d5b246cb3deeda02
Andrey Makarov <over.ph@gmail.com>
2011-12-22 12:14:42 +0400

    Add problems list ref to paragraph

--- app/controllers/q_problem_controller.rb
+++ app/controllers/q_problem_controller.rb
@@ -2,6 +2,19 @@ class QProblemController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @didactic_unit = QResource.find(params[:root_id])
+    @p_ref_d = Neo4j.query("START n=node({didactic_unit_id}), t=node({qproblem_type_id})
+                                  MATCH (n)<-[:refers_to]-(x)-[:is_type]->(t)
+                                  RETURN x",
+                           'didactic_unit_id' => @didactic_unit.id.to_i,
+                           'qproblem_type_id' => 123)
+    @p_ref_d_right = Neo4j.query("START n=node({didactic_unit_id}), u=node({user_id}), ts=node({qsolution_type_id}), tp=node({qproblem_type_id})
+                                  MATCH (n)<-[:refers_to]-(y)-[:is_type]->(tp), (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
+                                  RETURN y",
+                                 'didactic_unit_id' => @didactic_unit.id.to_i,
+                                 'user_id' => current_user.id.to_i,
+                                 'qsolution_type_id' => 126,
+                                 'qproblem_type_id' => 123)
   end
 
   def show
@@ -12,8 +25,8 @@ class QProblemController < ApplicationController
     @history_answers = Neo4j.query("START n=node({problem_id}), u=node({user_id})
                                   MATCH (n)-[:has_answer]->(x)<-[:chose_answer]-(u)
                                   RETURN x.body",
-                                 'problem_id' => @problem.id.to_i,
-                                 'user_id' => current_user.id.to_i)
+                                   'problem_id' => @problem.id.to_i,
+                                   'user_id' => current_user.id.to_i)
 
     if @problem.outgoing(:has_solution).depth(1).first != nil
       @solution = @problem.outgoing(:has_solution).depth(1).first.outgoing(:consists_of).sort_by(&:position)
@@ -37,8 +50,8 @@ class QProblemController < ApplicationController
     @taxonomy_level = Neo4j.query("START n=node({problem_id}), t=node({tax_level_id})
                                   MATCH (n)-[:refers_to]->(x)-[:is_type]->(t)
                                   RETURN x.title",
-                                 'problem_id' => @problem.id.to_i,
-                                 'tax_level_id' => 198)
+                                  'problem_id' => @problem.id.to_i,
+                                  'tax_level_id' => 198)
 
 
 
