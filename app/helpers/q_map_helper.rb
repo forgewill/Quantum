cabbb92b49a25c6fe692b0aeb5d6b720fa7e3aed
Andrey Makarov <over.ph@gmail.com>
2012-01-08 23:21:41 +0400

    Added Sum-Points helper

--- app/helpers/q_map_helper.rb
+++ app/helpers/q_map_helper.rb
@@ -25,7 +25,25 @@ module QMapHelper
     total.each do |s|
       sum = sum + QResource.find(s.first[1].id).weight
     end
-
-    html << sum.to_s
+    average = sum/total.count.to_f
+    html << average.to_s
   end
+  def sum_points(root)
+      html = ""
+
+      total = Neo4j.query("START n=node({root_id}), u=node({user_id}), ts=node({qsolution_type_id}), tp=node({qproblem_type_id})
+                                        MATCH (n)-[:contains*1..1000]->(p)<-[:refers_to]-(y)-[:is_type]->(tp), (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
+                                        RETURN y",
+                          'root_id' => root.to_i,
+                          'user_id' => current_user.id.to_i,
+                          'qsolution_type_id' => 126,
+                          'qproblem_type_id' => 123)
+
+      sum = 0
+      total.each do |s|
+        sum = sum + QResource.find(s.first[1].id).weight
+      end
+
+      html << sum.to_s
+    end
 end
