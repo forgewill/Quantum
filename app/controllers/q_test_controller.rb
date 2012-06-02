e27902346672c758b526617a70ac9f65cdae8764
Andrey Makarov <over.ph@gmail.com>
2012-06-02 13:53:58 +0400

    QTest enable to save info.

--- app/controllers/q_test_controller.rb
+++ app/controllers/q_test_controller.rb
@@ -1,5 +1,31 @@
 class QTestController < ApplicationController
   def index
+
+  end
+
+  def solve
+    index = 0
+
+    Neo4j::Transaction.run do
+      testid = QResource.find(params[:test_id])
+      @ssid = QResource.new(:ssid => params[:session_id])
+      @ssid.save!
+      user = User.find(current_user.id)
+      Neo4j::Relationship.new(:refers_to, @ssid, user)
+      Neo4j::Relationship.new(:refers_to, @ssid, testid)
+    end
+
+    params[:count_of_quests].to_i.times do
+      index += 1
+      Neo4j::Transaction.run do
+        answer = QResource.find(params["answer_"+index.to_s])
+        Neo4j::Relationship.new(:selected, @ssid, answer)
+      end unless params["answer_"+index.to_s].nil?
+    end
+    redirect_to :action => :report, :ssid => params[:session_id]
+  end
+
+  def show
     if params[:qtest_id] != nil
       @qtest = Hash.new
       @qtest["root"] = QResource.find(params[:qtest_id]) #Get QTest node
@@ -10,23 +36,8 @@ class QTestController < ApplicationController
                                     (x)-[:is_type]->(t)
                                     RETURN y",
                                             'qtest_id' => @qtest["root"].id.to_i,
-                                            '_ph_type_id' => 12)
-
-      #@qtest["_phs_list"] = @qtest["root"].outgoing(:consists_of)
+                                            '_ph_type_id' => 181)
     end
-  end
-
-  def solve
-    #Neo4j::Transaction.run do
-    #  answer = QResource.find(params[:user_chose_answer])
-    #  user = User.find(current_user.id)
-    #  Neo4j::Relationship.new(:chose_answer, user, answer)
-    #end
-    @res = 1 if params[:answer_1].empty?
-    redirect_to :action => :report, :res => @res
-  end
-
-  def show
   end
 
   def new
