a4b4397fdd55de5c699602628870d13e1a03a558
Andrey Makarov <over.ph@gmail.com>
2012-07-03 00:00:40 +0400

    Start FORMULA

--- app/controllers/q_test_controller.rb
+++ app/controllers/q_test_controller.rb
@@ -6,6 +6,7 @@ class QTestController < ApplicationController
 
   def solve
     index = 0
+    testid = ""
 
     Neo4j::Transaction.run do
       testid = QResource.find(params[:test_id])
@@ -23,7 +24,7 @@ class QTestController < ApplicationController
         Neo4j::Relationship.new(:selected, @ssid, answer)
       end unless params["answer_"+index.to_s].nil?
     end
-    redirect_to :action => :report, :ssid => params[:session_id]
+    redirect_to :action => :report, :ssid => params[:session_id], :qtest_id => testid
   end
 
   def show
@@ -57,7 +58,27 @@ class QTestController < ApplicationController
 
   def report
     #TODO Formula
-    @report = "Report!"
+    #0d1d82e43970ffc4105fabc53ba2c379
+    #9cfb13f2950ebafa3c4a0c8e95b4c830
+
+    test_id = params[:qtest_id].to_i
+    ssid = params[:ssid]
+    tphid = 12
+
+    phlist = Neo4j.query{ test = node(test_id); tph = node(tphid); ph = node; test > ':consists_of' > ph > ':is_type' > tph; ret(ph).asc(ph[:position]) }.to_a
+    phs = Array.new(phlist.count, {}) #phs[1][:id] = 25
+
+    prlist = Neo4j.query{ test = node(test_id); pr = node; ph = node; test > ':consists_of' > ph > ':hold_on' > pr; ret(pr, ph).asc(ph[:position]) }.to_a
+
+
+    phlist.each_with_index do |ph, i|
+      phs[i] = {}
+      phs[i][:id] = ph.first[1].id
+      #@report = @report.to_s + i.to_s + ph.first[1].id.to_s
+    end
+    phs[0][:weight] = 10
+    phs[1][:weight] = 15
+    @report = phs[1][:id].to_s + "|" + phs[0][:weight].to_s + "|" + phs[1][:weight].to_s
   end
 
 end
