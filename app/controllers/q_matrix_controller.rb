3f0aed51aa6d182309f53cb9bcd5f4fcc04265b3
Andrey Makarov <over.ph@gmail.com>
2012-08-03 00:59:52 +0400

    MLS #1

--- app/controllers/q_matrix_controller.rb
+++ app/controllers/q_matrix_controller.rb
@@ -1,5 +1,18 @@
 class QMatrixController < ApplicationController
   def index
+    @qmatrix = {}
+    disc_id = 59
+    theme_type_id = 110
+
+    matrix = Neo4j.query{ discipline = node(disc_id); chapter = node; theme = node; theme_type = node(theme_type_id); discipline > ':contains' > chapter > ':contains' > theme; theme > ':is_type' > theme_type; ret(theme).asc(theme[:note]) }.to_a
+
+    marray = []
+    matrix.each_with_index do |theme,i|
+      marray[i] = {:note => theme.first[1][:note].to_i, :id => theme.first[1].id}
+    end
+    marray = marray.sort_by {|hsh| hsh[:note]}
+
+    @qmatrix[:m] = marray
   end
 
   def tree
--- app/helpers/q_map_helper.rb
+++ app/helpers/q_map_helper.rb
@@ -13,41 +13,42 @@ module QMapHelper
   def average_points(root)
     html = ""
 
-    if QResource.find(root).outgoing(:is_type).first != nil
-      if QResource.find(root).outgoing(:is_type).first.title == 'Type_QParagraph'
-        total = Neo4j.query("START n=node({root_id}), u=node({user_id}), ts=node({qsolution_type_id}), tp=node({qproblem_type_id})
-                                              MATCH (n)<-[:refers_to]-(y)-[:is_type]->(tp), (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
-                                              RETURN y",
-                            'root_id' => root.to_i,
-                            'user_id' => current_user.id.to_i,
-                            'qsolution_type_id' => 126,
-                            'qproblem_type_id' => 123)
-      else
-        total = Neo4j.query("START n=node({root_id}), u=node({user_id}), ts=node({qsolution_type_id}), tp=node({qproblem_type_id})
-                                                    MATCH (n)-[:contains*1..1000]->(p)<-[:refers_to]-(y)-[:is_type]->(tp), (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
-                                                    RETURN y",
-                            'root_id' => root.to_i,
-                            'user_id' => current_user.id.to_i,
-                            'qsolution_type_id' => 126,
-                            'qproblem_type_id' => 123)
-      end
-    else
-      total = Neo4j.query("START n=node({root_id}), u=node({user_id}), ts=node({qsolution_type_id}), tp=node({qproblem_type_id})
-                                            MATCH (n)-[:contains*1..1000]->(p)<-[:refers_to]-(y)-[:is_type]->(tp), (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
-                                            RETURN y",
-                          'root_id' => root.to_i,
-                          'user_id' => current_user.id.to_i,
-                          'qsolution_type_id' => 126,
-                          'qproblem_type_id' => 123)
-    end
-
-
-    sum = 0
-    total.each do |s|
-      sum = sum + QResource.find(s.first[1].id).weight
-    end
-    average = sum/total.count.to_f
-
+    #TODO Need to switch to new query syntax
+    #if QResource.find(root).outgoing(:is_type).first != nil
+    #  if QResource.find(root).outgoing(:is_type).first.title == 'Type_QParagraph'
+    #    total = Neo4j.query("START n=node({root_id}), u=node({user_id}), ts=node({qsolution_type_id}), tp=node({qproblem_type_id})
+    #                                          MATCH (n)<-[:refers_to]-(y)-[:is_type]->(tp), (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
+    #                                          RETURN y",
+    #                        'root_id' => root.to_i,
+    #                        'user_id' => current_user.id.to_i,
+    #                        'qsolution_type_id' => 126,
+    #                        'qproblem_type_id' => 123)
+    #  else
+    #    total = Neo4j.query("START n=node({root_id}), u=node({user_id}), ts=node({qsolution_type_id}), tp=node({qproblem_type_id})
+    #                                                MATCH (n)-[:contains*1..1000]->(p)<-[:refers_to]-(y)-[:is_type]->(tp), (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
+    #                                                RETURN y",
+    #                        'root_id' => root.to_i,
+    #                        'user_id' => current_user.id.to_i,
+    #                        'qsolution_type_id' => 126,
+    #                        'qproblem_type_id' => 123)
+    #  end
+    #else
+    #  total = Neo4j.query("START n=node({root_id}), u=node({user_id}), ts=node({qsolution_type_id}), tp=node({qproblem_type_id})
+    #                                        MATCH (n)-[:contains*1..1000]->(p)<-[:refers_to]-(y)-[:is_type]->(tp), (y)-[:has_answer]->(x)-[:is_type]->(ts), (u)-[:chose_answer]->(x)
+    #                                        RETURN y",
+    #                      'root_id' => root.to_i,
+    #                      'user_id' => current_user.id.to_i,
+    #                      'qsolution_type_id' => 126,
+    #                      'qproblem_type_id' => 123)
+    #end
+    #
+    #
+    #sum = 0
+    #total.each do |s|
+    #  sum = sum + QResource.find(s.first[1].id).weight
+    #end
+    #average = sum/total.count.to_f
+    average = "n/a"
     html << average.to_s
   end
 
